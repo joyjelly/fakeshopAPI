117d6e18acb29c68e304c49132228c31c84d7565
joyjelly <noop0728@naver.com>
2024-09-23 15:01:07 +0900

    Initial commit

---
+++ src/main/java/com/fakeshopAPI/FakeshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.fakeshopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeshopApiApplication.class, args);
+	}
+
+}
